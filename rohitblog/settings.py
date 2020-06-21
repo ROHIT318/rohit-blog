2b0ae4a916ce5708a9b5dfbbcd49331a412c7b49
ROHIT SHARMA <38538605+ROHIT318@users.noreply.github.com>
2020-06-21 19:58:46 +0530

    updated database credentials

--- rohitblog/settings.py
+++ rohitblog/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'rohitblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ddanudj95g67sn',
+        'USER': 'dpzyveuhqwsogd',
+        'PASSWORD': '70e036851da332d3348be111c14327535597ff3c9043d3f7bde634c9667384e9',
+        'HOST': 'ec2-18-214-211-47.compute-1.amazonaws.com',
     }
 }
 
