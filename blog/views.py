1ff0d8202594b750619f5c94f78530012480600a
ROHIT SHARMA <38538605+ROHIT318@users.noreply.github.com>
2020-06-21 19:55:15 +0530

    updated views for registration and login

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
 
 
 # Create your views here.
@@ -6,7 +7,24 @@ def home(request):
 	return render(request, 'index.html')
 
 def signup(request):
-	return render(request, 'signup.htm')
+	if request.method == 'POST':
+		inputFname = request.POST['inputFname']
+		inputLname = request.POST['inputLname']
+		userName = inputFname + inputLname
+		email = request.POST['inputEmail1']
+		password1 = request.POST['inputPassword2']
+		user = User.objects.create_user(username = userName, password = password1, email = email, first_name = inputFname, last_name = inputLname)
+		user.save()
+		return render(request, 'login.htm')
+	else:
+		return render(request, 'signup.htm')
 
 def login(request):
-	return render(request, 'login.htm')
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = auth.authenticate(username = username, password = password)
+		if user is not None:
+			return render(request, 'index.html')
+	else:
+		return render(request, 'login.htm')
